6626fe531bae0e62c64467c9a96cb4ebe7a0d0f1
Clément Vannouque <clement.vannouque@gmail.com>
2025-08-01 10:01:28 -0400

    feat: video link component (#34)
    * decouple CardSmall and CardWide components from race object
    
    * wip VideoLink component
    
    * finish video link component
    
    * update img
    
    * refac play icon

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -587,5 +587,14 @@ export const Icons = {
     viewBox: "0 0 24 24",
     fill: "currentColor",
   },
-  // <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d=/></svg>
+  play: {
+    paths: [
+      {
+        d: "M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.647c1.295.742 1.295 2.545 0 3.286L7.279 20.99c-1.25.717-2.779-.217-2.779-1.643V5.653z",
+      },
+    ],
+    class: "h-6 w-6 shrink-0 fill-current",
+    viewBox: "0 0 24 24",
+    fill: "currentColor",
+  },
 };
