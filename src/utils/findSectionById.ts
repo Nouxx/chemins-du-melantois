2003834ed3a94475e22c0f84d791390dfce79cc4
Nouxx <clement.vannouque@gmail.com>
2025-09-16 23:32:11 +0200

    fix build by removing sh*t

--- src/utils/findSectionById.ts
+++
@@ -1,7 +0,0 @@
-import type { Race, SectionId } from "@/data_files/constants";
-
-export const findSectionById = (race: Race, id: SectionId) => {
-  const section = race.sections.find((section) => section.id === id);
-  if (!section) throw new Error(`Section with id ${id} not found`);
-  return section;
-};
