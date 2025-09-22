c6ee4a5ebbd1f402c54ed69c1cda0a91055adc05
Nouxx <clement.vannouque@gmail.com>
2025-09-22 23:10:17 +0200

    move routes to its own folder

--- src/data_files/navigation/routes/routes.ts
+++ src/data_files/navigation/routes/routes.ts
@@ -1,4 +1,4 @@
-import { RoutesSchema } from "@/data_files/navigation/schema";
+import { RoutesSchema } from "@/data_files/navigation/routes/schema";
 import type { RaceUrlSlug } from "@/data_files/races/slugs";
 import { SiteMetadata } from "@/data_files/site/metadata";
 
