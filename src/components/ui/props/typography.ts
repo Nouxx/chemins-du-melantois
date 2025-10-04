c79ae90289766f2161ad91ee2ca927bd085aac79
Nouxx <clement.vannouque@gmail.com>
2025-10-04 15:33:13 +0200

    add text color variants

--- src/components/ui/props/typography.ts
+++ src/components/ui/props/typography.ts
@@ -1,4 +1,11 @@
 export const fontWeightVariants = {
   normal: "font-normal",
   bold: "font-bold",
-};
+} as const;
+
+export const textColorVariants = {
+  grey: "text-neutral-600",
+  black: "text-neutral-700",
+  lightGreen: "text-brand-light-green",
+  white: "text-neutral-50",
+} as const;
