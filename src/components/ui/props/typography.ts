c1dfbdb665f55d9006a3dc8887d9b2f0c2fa9568
Clément Vannouque <clement.vannouque@gmail.com>
2025-10-06 06:55:51 -0400

    fix: minor UI adjustments (#49)
    * update text colors
    
    * fix wording

--- src/components/ui/props/typography.ts
+++ src/components/ui/props/typography.ts
@@ -5,7 +5,8 @@ export const fontWeightVariants = {
 
 export const textColorVariants = {
   grey: "text-neutral-600",
-  black: "text-neutral-700",
+  darkGrey: "text-neutral-700",
+  black: "text-neutral-800",
   lightGreen: "text-brand-light-green",
   white: "text-neutral-50",
 } as const;
