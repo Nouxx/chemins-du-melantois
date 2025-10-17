b98e6f215380e03e8b7d272dfc407778ce518f6e
Nouxx <clement.vannouque@gmail.com>
2025-10-17 21:42:54 +0200

    wip shit

--- src/components/ui/atoms/Translation.test.ts
+++ src/components/ui/atoms/Translation.test.ts
@@ -36,3 +36,50 @@ test("should render <color>", async () => {
     'Bienvenue aux Chemins du <span class="text-brand-light-green">Mélantois</span>',
   );
 });
+
+test("should throw when no input or key are provided", async () => {
+  const container = await AstroContainer.create();
+
+  await expect(
+    container.renderToString(Translation, {
+      props: {},
+    }),
+  ).rejects.toThrow();
+});
+
+test("should throw when there is an additional closing tag", async () => {
+  const container = await AstroContainer.create();
+
+  await expect(
+    container.renderToString(Translation, {
+      props: {
+        input: "This is a <color>text</color></strong>",
+      },
+    }),
+  ).rejects.toThrow();
+});
+
+test("should throw when there is an additional opening tag", async () => {
+  const container = await AstroContainer.create();
+
+  await expect(
+    container.renderToString(Translation, {
+      props: {
+        input: "This <strong>is a <color>text</color>",
+      },
+    }),
+  ).rejects.toThrow();
+});
+
+test.only("should YE", async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(Translation, {
+    props: {
+      input:
+        "This is a <strong>Strong Text</strong> and a <strong>A <strong>Very strong</strong></strong> and a <italic><strong>Strong and italic</strong> here</italic> and a <color>Colored and <strong>Strong</strong></color> text.",
+    },
+  });
+
+  expect(result).toBe(`This is a <span class="font-bold">Strong Text</span> and a <span class="font-bold">A <span class="font-bold">Very strong</span></span> and a <span class="italic"><span class="font-bold">Strong and italic</span> here</span> and a <span class="text-brand-light-green">Colored and <span class="font-bold">Strong</span></span> text.`,
+);
+});
