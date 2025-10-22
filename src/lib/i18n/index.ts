f6f8eadf6406d728f03d1e3de6a195079ec8863a
Nouxx <clement.vannouque@gmail.com>
2025-10-22 13:07:57 +0200

    wip

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -2,6 +2,9 @@ import { getTranslationForKey } from "./getTranslationForKey";
 import type { SupportedLanguages, TemplateVariables } from "./types";
 import { getInterpolatedTranslation } from "./getInterpolatedTranslation";
 import { getTranslations } from "./getTranslations";
+import frenchTranslations from "@data/translations/fr.json";
+
+import i18next from "i18next";
 
 export const createTranslator = (lang: SupportedLanguages) => {
   let translations = getTranslations(lang);
@@ -21,3 +24,15 @@ export const createTranslator = (lang: SupportedLanguages) => {
 };
 
 export const t = createTranslator("fr");
+
+await i18next.init({
+  lng: "fr",
+  debug: true,
+  resources: {
+    fr: {
+      translation: frenchTranslations,
+    },
+  },
+});
+
+export const t2 = i18next.t;
